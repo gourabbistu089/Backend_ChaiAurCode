b1e795003a4ff7e8fb55c5133cad0b38329a8e0b
Gourab <gourabbistu2002@gmail.com>
2024-09-08 00:56:57 +0530

    chapter 08

--- 08_apiResponse_errorHandling/src/index.js
+++ 08_apiResponse_errorHandling/src/index.js
@@ -32,6 +32,12 @@ const app = express();
 
 // require("dotenv").config({ path: "./env" }); correct
 import 'dotenv/config';
-
+import {app} from "./app.js";
 import connectDB from "./db/index.js";
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port 8000`);
+  })
+}).catch((error) => {
+  console.log("DB connection failed", error); 
+})
